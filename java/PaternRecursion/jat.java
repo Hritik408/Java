43695dff18538c3ae7dbd780b18e3fb26c9f1f3d
Hritik408 <chaudharyhritik91@gmail.com>
2024-01-17 23:50:30 +0530

    .

---
+++ java/PaternRecursion/jat.java
@@ -0,0 +1,9 @@
+package PaternRecursion;
+
+public class jat{
+    public static void main(String[] args) {
+        System.out.println("hritik");
+        System.out.println("chaudhary");
+        System.out.println("slystem");
+    }
+}
